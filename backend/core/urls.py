87e9feb43a5259b5e8702cc11b53b04e9a7fc8d9
Mickey Hefley <0xCODEs@users.noreply.github.com>
2020-01-25 08:55:53 -0600

    URL update on server

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('21232f297a57a5a743894a0e4a801fc3/', admin.site.urls),
     path('api/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    re_path(r'^',csrf_exempt(TemplateView.as_view(template_name='index.html')))
+    # re_path(r'^',csrf_exempt(TemplateView.as_view(template_name='index.html')))
 ]
